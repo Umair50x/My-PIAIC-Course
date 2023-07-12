a376df5c4b8d08cac4e2c88bdb0e2a337c8c5e5f
Umair50x <i2u1@outlook.com>
2023-07-12 13:19:47 +0500

    Add four var to add,sub,mult & divide and log to console.

---
+++ piaic-course/july-2023/getting-started-typescript/numberEight.ts
@@ -0,0 +1,17 @@
+
+
+
+//Number Eight: 
+//Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+//Be sure to enclose your operations in print statements to see the results.
+
+let add = 2 + 2;
+let subtract = 2 - 2;
+let multiply = 2 * 3;
+let divide = 6 / 3;
+
+console.log(add);
+console.log(subtract);
+console.log(multiply);
+console.log(divide);
+
