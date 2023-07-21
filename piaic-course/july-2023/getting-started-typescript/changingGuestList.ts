c8657d2b8776a3c2bc7f3c0ac04cf5818d111b59
Umair50x <i2u1@outlook.com>
2023-07-21 11:20:50 +0500

    Add export {} to turn this file into a module and use it immediately in moreGuests.ts. All code will be export with curly brackets.

--- piaic-course/july-2023/getting-started-typescript/changingGuestList.ts
+++ piaic-course/july-2023/getting-started-typescript/changingGuestList.ts
@@ -2,7 +2,7 @@
 //Changing Guest List: You just heard that one of your guests can’t make the dinner, 
 //so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
 
-// • Start with your program from Exercise 14. 
+// • Start with your program from Exercise 14. //didn't know abt export import yet
 //Add a print statement at the end of your program stating the name of the guest who can’t make it.
 // • Modify your list, 
 //replacing the name of the guest who can’t make it with the name of the new person you are inviting.
@@ -38,3 +38,6 @@ console.log("\nNEW INVITATION LIST AND THEIR MESSAGES\n");
     };
 
 }
+
+
+export {};
