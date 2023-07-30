33a98dc5d0385fd09c7cac005da10a935d196035
Ummi <i2u1@outlook.com>
2023-07-30 20:19:20 +0500

    Add new dir 'src' to hold all source code .ts files

---
+++ piaic-course/july-2023/getting-started-typescript/src/favoriteNumber.ts
@@ -0,0 +1,11 @@
+
+
+
+//Favorite Number: Store your favorite number in a variable. 
+//Then, using that variable, create a message that reveals your favorite number. 
+//Print that message.
+
+let favNum = "07";
+let favNumMsg = " is my favorite number.";
+
+console.log(favNum + favNumMsg);
