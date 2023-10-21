b53e1c1aecac561bdcc580a0ea8ab4df420f845b
Ummi <i2u1@outlook.com>
2023-10-21 12:50:06 +0500

    Renamed file to the specific order as per the repo..

---
+++ piaic-course/july-2023/getting-started-typescript/src/04famousQuote.ts
@@ -0,0 +1,11 @@
+
+
+//Famous Quote: Find a quote from a famous person you admire. 
+//Print the quote and the name of its author. 
+//Your output should look something like the following, including the quotation marks:
+//
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+let famousPerson = "Albert Einstein";
+let famousPersonQuote = "A person who never made a mistake never tried anything new.";
+console.log(famousPerson + " once said, \"" + famousPersonQuote + "\".");
