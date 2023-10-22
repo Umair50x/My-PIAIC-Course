5cf9eb400379677d376231172c7e3c177bf362bd
Ummi <i2u1@outlook.com>
2023-10-22 18:46:27 +0500

    Add 10 simple conditional tests

--- piaic-course/july-2023/getting-started-typescript/src/23conditionalTests.ts
+++ piaic-course/july-2023/getting-started-typescript/src/23conditionalTests.ts
@@ -1,1 +1,64 @@
 #!/usr/bin/env node
+// Test 1
+let car = "subaru";
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == "subaru"); // True
+
+// Test 2
+let age = 25;
+console.log("Is age > 18? I predict True.");
+console.log(age > 18); // True
+
+// Test 3
+let isRaining = true;
+console.log("Is it raining? I predict True.");
+console.log(isRaining); // True
+
+// Test 4
+let city = "Los Angeles";
+console.log("Is city == 'Los Angeles'? I predict True.");
+console.log(city == "Los Angeles"); // True
+
+// Test 5
+let score = 80;
+console.log("Is score < 60? I predict False.");
+console.log(score < 60); // False
+
+// Test 6
+let isSunny = false;
+console.log("Is it sunny? I predict False.");
+console.log(isSunny); // False
+
+// Test 7
+let fruit = "apple";
+console.log("Is fruit == 'banana'? I predict False.");
+console.log(fruit == "banana"); // False
+
+// Test 8
+let temperature = 30;
+console.log("Is temperature >= 32? I predict False.");
+console.log(temperature >= 32); // False
+
+// Test 9
+let dayOfWeek = "Monday";
+console.log("Is dayOfWeek == 'Friday'? I predict False.");
+console.log(dayOfWeek == "Friday"); // False
+
+// Test 10
+let hasPermission = true;
+console.log("Does the user have permission? I predict True.");
+console.log(hasPermission); // True
+
+// Conditional Tests: Write a series of conditional tests.
+// Print a statement describing each test and your prediction
+// for the results of each test. Your code should look something like this:
+
+// let car = 'subaru';
+
+// console.log("Is car == 'subaru'? I predict True.")
+
+// console.log(car == 'subaru')
+
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
