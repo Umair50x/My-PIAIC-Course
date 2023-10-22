2795f0c119d8ef5155fe2529124fadfd36b0f54a
Ummi <i2u1@outlook.com>
2023-10-22 12:51:20 +0500

    Write a program that has an array of things and print it in a list type in console.

--- piaic-course/july-2023/getting-started-typescript/src/20arrayStorage.ts
+++ piaic-course/july-2023/getting-started-typescript/src/20arrayStorage.ts
@@ -1,1 +1,20 @@
 #!/usr/bin/env node
+
+const arrayOfThings: string[] = [
+  "Everest",
+  "Nile",
+  "Canada",
+  "Makkah",
+  "Farsi",
+];
+
+console.log("list of things: ");
+
+arrayOfThings.forEach((thing, index) => {
+  console.log(`${index + 1}: ${thing}`);
+});
+
+//Think of something you could store in a array.
+//For example, you could make a list of mountains,
+//rivers, countries, cities, languages, or anything else you’d like.
+//Write a program that creates a list containing these items.
