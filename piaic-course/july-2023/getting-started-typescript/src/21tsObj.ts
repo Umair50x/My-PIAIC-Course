41c01e114b085d786ad6a5fa8be72f9c85957a4d
Ummi <i2u1@outlook.com>
2023-10-22 13:03:12 +0500

    Write an object of things and print it to the console as a list.

--- piaic-course/july-2023/getting-started-typescript/src/21tsObj.ts
+++ piaic-course/july-2023/getting-started-typescript/src/21tsObj.ts
@@ -1,1 +1,20 @@
 #!/usr/bin/env node
+
+ const objOfThings = {
+    mountain: "Everest",
+    river: "Nile",
+    country: "Canada",
+    city: "Makkah",
+    language: "Farsi",
+}
+
+console.log("An Object of things: ");
+console.log(`mountain: ${objOfThings.mountain}`);
+console.log(`river: ${objOfThings.river}`);
+console.log(`country: ${objOfThings.country}`);
+console.log(`city: ${objOfThings.city}`);
+console.log(`language: ${objOfThings.language}`);
+
+
+//They think of something you could store in a TypeScript Object. 
+//Write a program that creates Objects containing these items.
