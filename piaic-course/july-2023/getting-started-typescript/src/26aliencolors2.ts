67311f7f9733cc94dbbdd46aaa0cdf51cce7e80e
Ummi <i2u1@outlook.com>
2023-10-23 21:44:05 +0500

    Add two alien color if conditions

--- piaic-course/july-2023/getting-started-typescript/src/26aliencolors2.ts
+++ piaic-course/july-2023/getting-started-typescript/src/26aliencolors2.ts
@@ -1,1 +1,23 @@
 #!/usr/bin/env node
+let alien_color = 'green';
+
+if (alien_color === 'green') {
+  console.log("The player just earned 5 points for shooting the alien.");
+} else {
+  console.log("The player just earned 10 points.");
+}
+
+let alien_color2 = 'red';
+
+if (alien_color2 === 'green') {
+  console.log("The player just earned 5 points for shooting the alien.");
+} else {
+  console.log("The player just earned 10 points.");
+}
+//Alien Colors #2: Choose a color for an alien as you did in Exercise 25, 
+//and write an if-else chain.
+// • If the alien’s color is green, print a statement that the player just 
+//earned 5 points for shooting the alien.
+// • If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+// • Write one version of this program that runs the if block and another that runs the else block.
+
