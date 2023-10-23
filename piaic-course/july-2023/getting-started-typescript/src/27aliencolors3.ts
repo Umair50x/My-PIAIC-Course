bc59e0d0687fbed8e46bb24cf77de635309bd0de
Ummi <i2u1@outlook.com>
2023-10-23 21:48:00 +0500

    Add 3 versions of alien color if conditions

--- piaic-course/july-2023/getting-started-typescript/src/27aliencolors3.ts
+++ piaic-course/july-2023/getting-started-typescript/src/27aliencolors3.ts
@@ -1,1 +1,35 @@
 #!/usr/bin/env node
+
+const alien_color = 'green';
+
+if (alien_color === 'green') {
+  console.log("The player earned 5 points.");
+} else if (alien_color === 'yellow') {
+  console.log("The player earned 10 points.");
+} else if (alien_color === 'red') {
+  console.log("The player earned 15 points.");
+}
+const alien_color2 = 'yellow';
+
+if (alien_color2 === 'green') {
+  console.log("The player earned 5 points.");
+} else if (alien_color2 === 'yellow') {
+  console.log("The player earned 10 points.");
+} else if (alien_color2 === 'red') {
+  console.log("The player earned 15 points.");
+}
+const alien_color3 = 'red';
+
+if (alien_color3 === 'green') {
+  console.log("The player earned 5 points.");
+} else if (alien_color3 === 'yellow') {
+  console.log("The player earned 10 points.");
+} else if (alien_color3 === 'red') {
+  console.log("The player earned 15 points.");
+}
+
+// //Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+// • If the alien is green, print a message that the player earned 5 points.
+// • If the alien is yellow, print a message that the player earned 10 points.
+// • If the alien is red, print a message that the player earned 15 points.
+// • Write three versions of this program, making sure each message is printed for the appropriate color alien.
