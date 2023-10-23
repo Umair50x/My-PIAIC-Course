e9908b4f88b3d88fcbda3c91d36cdbaeb8d4f5ae
Ummi <i2u1@outlook.com>
2023-10-24 01:29:01 +0500

    Print msg indicating num of people invited to the dinner

--- piaic-course/july-2023/getting-started-typescript/src/19dinnerGuests.ts
+++ piaic-course/july-2023/getting-started-typescript/src/19dinnerGuests.ts
@@ -1,1 +1,7 @@
 #!/usr/bin/env node
+
+console.log("This exercise is completely unnecessary");
+
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, 
+//print a message indicating the number of people you are inviting to dinner.
+
