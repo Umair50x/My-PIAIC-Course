947af705cd2e8c91e92cab355da812661b27dbb1
Ummi <i2u1@outlook.com>
2023-10-23 23:41:55 +0500

    Add city country pairs func

--- piaic-course/july-2023/getting-started-typescript/src/39cityNames.ts
+++ piaic-course/july-2023/getting-started-typescript/src/39cityNames.ts
@@ -1,1 +1,15 @@
 #!/usr/bin/env node
+
+function cityCountry(city: string, country: string) {
+  let cons = console.log(`${city}, ${country}`);
+  return cons;
+}
+
+cityCountry("lahore", "pakistan");
+cityCountry("Karachi", "pakistan");
+cityCountry("Islamabad", "pakistan");
+//City Names: Write a function called city_country() that
+//takes in the name of a city and its country.
+//The function should return a string formatted like this:
+// "Lahore, Pakistan"
+// Call your function with at least three city-country pairs, and print the value that’s returned.
