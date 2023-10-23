ee7d67fc5f8a1a6234328b0ebeefadcfb7d68712
Ummi <i2u1@outlook.com>
2023-10-23 23:38:32 +0500

    Add a func of city and country

--- piaic-course/july-2023/getting-started-typescript/src/38cities.ts
+++ piaic-course/july-2023/getting-started-typescript/src/38cities.ts
@@ -1,1 +1,12 @@
 #!/usr/bin/env node
+
+function describeCity(city: string, country: string = "pakistan") {
+  console.log(`${city} is in ${country}`);
+}
+describeCity("karachi");
+describeCity("Lahore");
+describeCity("sydney");
+// Cities: Write a function called describe_city() that accepts the name of a city
+//and its country. The function should print a simple sentence, such as Karachi
+//is in Pakistan. Give the parameter for the country a default value.
+//Call your function for three different cities, at least one of which is not in the default country.
