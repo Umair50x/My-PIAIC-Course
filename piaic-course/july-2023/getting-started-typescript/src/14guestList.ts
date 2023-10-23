c0961d5e400e2815d9e0979e25ec173745cbe2ef
Ummi <i2u1@outlook.com>
2023-10-24 01:31:19 +0500

    Add guest list for dinner

--- piaic-course/july-2023/getting-started-typescript/src/14guestList.ts
+++ piaic-course/july-2023/getting-started-typescript/src/14guestList.ts
@@ -1,27 +1,17 @@
 #!/usr/bin/env node
 
+let guestInvList = ["person1", "person2", "person3"];
 
+let preMsg = "Dear ";
 
-//Guest List: If you could invite anyone, living or deceased, to dinner, 
+let msgToGuest =
+  ", I am inviting you for a late-night steak dinner at my home. Please come.";
+
+console.log(preMsg + guestInvList[0] + msgToGuest);
+console.log(preMsg + guestInvList[1] + msgToGuest);
+console.log(preMsg + guestInvList[2] + msgToGuest);
+
+export { guestInvList, preMsg, msgToGuest };
+//Guest List: If you could invite anyone, living or deceased, to dinner,
 //who would you invite? Make a list that includes at least three people you’d like to invite to dinner.
 //Then use your list to print a message to each person, inviting them to dinner.
-
-let guestInvList = ["this Person", "that Person", "Person 3", 
-                    "Person four", "Person 5th", "Sixth Person", "Person Seven"];
-let preMsg = "Dear ";
-let msgToThisPerson = ", I am inviting you for a late-night steak dinner at my home. Please come.";
-let msgToThatPerson = ", I am inviting you for a late-night steak dinner at my home. Please come.";
-let msgToPerson3 = ", I am inviting you for a late-night steak dinner at my home. Please come.";
-let msgToPersonFour = ", I am inviting you for a late-night steak dinner at my home. Please come.";
-let msgTPerson5th = ", I am inviting you for a late-night steak dinner at my home. Please come.";
-let msgToSixthPerson = ", I am inviting you for a late-night steak dinner at my home. Please come.";
-let msgToPersonSeven = ", I am inviting you for a late-night steak dinner at my home. Please come.";
-
-console.log(preMsg + guestInvList[0] +  msgToThisPerson);
-console.log(preMsg + guestInvList[1] +  msgToThatPerson);
-console.log(preMsg + guestInvList[2] +  msgToPerson3);
-console.log(preMsg + guestInvList[3] +  msgToPersonFour);
-console.log(preMsg + guestInvList[4] +  msgTPerson5th);
-console.log(preMsg + guestInvList[5] +  msgToSixthPerson);
-console.log(preMsg + guestInvList[6] +  msgToPersonSeven);
-
