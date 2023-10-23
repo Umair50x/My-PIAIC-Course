d09e28c47dcfb3be3428aa958eb41e4cc55f87ee
Ummi <i2u1@outlook.com>
2023-10-23 23:05:47 +0500

    Add pizza array and loop it to print a sentence

--- piaic-course/july-2023/getting-started-typescript/src/34pizzas.ts
+++ piaic-course/july-2023/getting-started-typescript/src/34pizzas.ts
@@ -1,1 +1,17 @@
 #!/usr/bin/env node
+
+const pizzas: string[] = ["piza1", "piza2", "piza3"];
+
+for (const pizzaName of pizzas) {
+  console.log(`I really like ${pizzaName}`);
+}
+console.log("I love pizzas!");
+
+//Pizzas: Think of at least three kinds of your favorite pizza.
+//Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+// • Modify your for loop to print a sentence using the name of the
+//pizza instead of printing just the name of the pizza. For each pizza you
+//should have one line of output containing a simple statement like I like pepperoni pizza.
+// • Add a line at the end of your program, outside the for loop, that states
+//how much you like pizza. The output should consist of three or more lines
+//about the kinds of pizza you like and then an additional sentence, such as I really love pizza!
