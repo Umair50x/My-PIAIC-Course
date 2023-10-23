ed05029fc057e2fdd220c2cdc00d983274d0c513
Ummi <i2u1@outlook.com>
2023-10-24 00:54:57 +0500

    Add magician array and loop it

--- piaic-course/july-2023/getting-started-typescript/src/41magicians.ts
+++ piaic-course/july-2023/getting-started-typescript/src/41magicians.ts
@@ -1,1 +1,13 @@
 #!/usr/bin/env node
+
+export const magiciansArr: string[] = ["magician1", "magician2", "magician3"];
+export function showMagician(magicians: string[]) {
+  magicians.forEach((name) => {
+    console.log(name);
+  });
+}
+
+showMagician(magiciansArr);
+//Magicians: Make a array of magician’s names.
+//Pass the array to a function called show_magicians(),
+//which prints the name of each magician in the array.
--- piaic-course/july-2023/getting-started-typescript/src/42greatMagicians.ts
+++ piaic-course/july-2023/getting-started-typescript/src/42greatMagicians.ts
@@ -1,1 +1,16 @@
 #!/usr/bin/env node
+
+import { magiciansArr, showMagician } from "./41magicians.js";
+
+function makeGreatMagician(magicians: string[]) {
+  for (let i = 0; i < magicians.length; i++) {
+    magicians[i] = "The Great " + magicians[i];
+  }
+}
+makeGreatMagician(magiciansArr);
+showMagician(magiciansArr);
+
+//Great Magicians: Start with a copy of your program from Exercise 39.
+//Write a function called make_great() that modifies the array of magicians
+// by adding the phrase the Great to each magician’s name. Call show_magicians()
+//to see that the list has actually been modified.
