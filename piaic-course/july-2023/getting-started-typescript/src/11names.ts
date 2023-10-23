30bb6b79515e8307ecc84a18474de7f7dd597f78
Ummi <i2u1@outlook.com>
2023-10-24 01:32:44 +0500

    Add an array of names and print them

--- piaic-course/july-2023/getting-started-typescript/src/11names.ts
+++ piaic-course/july-2023/getting-started-typescript/src/11names.ts
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
-//Names: Store the names of a few of your friends in a array called names.
-//Print each person’s name by accessing each element in the list, one at a time.
-let anArray: string[] = ["friend1", "friend2", "friend3", "friend4"];
+export const  anArray: string[] = ["friend1", "friend2", "friend3", "friend4"];
 
-function logArrValues() {
+export function logArrValues() {
   console.log(anArray[0]);
   console.log(anArray[1]);
   console.log(anArray[2]);
   console.log(anArray[3]);
 }
-logArrValues();
-export { anArray, logArrValues };
+//Names: Store the names of a few of your friends in a array called names.
+//Print each person’s name by accessing each element in the list, one at a time.
