7662803c63ff0e0341677d4ed3e2a1b57a111e93
Ummi <i2u1@outlook.com>
2023-10-24 01:06:27 +0500

    Add a func that utilizes ...rest params

--- piaic-course/july-2023/getting-started-typescript/src/44sandwiches.ts
+++ piaic-course/july-2023/getting-started-typescript/src/44sandwiches.ts
@@ -1,1 +1,17 @@
 #!/usr/bin/env node
+
+function makeSandwich(...sandwichItems: string[]) {
+  console.log("\nSandwich Order: ");
+  for (const item of sandwichItems) {
+    console.log(`We put a ${item}`);
+  }
+}
+
+makeSandwich("omelete", "cucumber", "salad", "potato");
+makeSandwich("Bread", "Ham", "Cheese", "Lettuce");
+makeSandwich("Bread", "Turkey", "Tomato");
+
+//Sandwiches: Write a function that accepts a array of items a person wants on a sandwich.
+//The function should have one parameter that collects as many items as the function call provides,
+// and it should print a summary of the sandwich that is being ordered. Call the function three times,
+//using a different number of arguments each time.
