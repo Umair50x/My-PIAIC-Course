068d7bb0081bad34f0623f4f47e748db6c7a0c85
Ummi <i2u1@outlook.com>
2023-10-23 23:33:36 +0500

    Make func makeShirt() with size and msg params

--- piaic-course/july-2023/getting-started-typescript/src/36tShirts.ts
+++ piaic-course/july-2023/getting-started-typescript/src/36tShirts.ts
@@ -1,1 +1,12 @@
 #!/usr/bin/env node
+
+function makeShirt(size: number, msg: string) {
+  console.log(`Shirt size: ${size}`);
+  console.log(`Message to print: ${msg}`);
+}
+
+makeShirt(15, "I love JS");
+//T-Shirt: Write a function called make_shirt() that accepts a size and
+// the text of a message that should be printed on the shirt.
+//The function should print a sentence summarizing the size of the
+//shirt and the message printed on it. Call the function.
--- piaic-course/july-2023/getting-started-typescript/src/37largeShirts.ts
+++ piaic-course/july-2023/getting-started-typescript/src/37largeShirts.ts
@@ -1,1 +1,22 @@
 #!/usr/bin/env node
+
+// import makeShirt from "./36tShirts.js";
+
+function makeShirt(size = "Large", message = "I love TypeScript") {
+  console.log(`T-shirt size: ${size}`);
+  console.log(`Message: ${message}`);
+}
+
+// Make a large shirt with the default message
+makeShirt();
+
+// Make a medium shirt with the default message
+makeShirt("Medium");
+
+// Make a small shirt with a custom message
+makeShirt("Small", "Small Shirt of mine");
+
+// Large Shirts: Modify the make_shirt() function so that shirts
+// are large by default with a message that reads I love TypeScript.
+// Make a large shirt and a medium shirt with the default message,
+// and a shirt of any size with a different message.
