474d3b21600ee6a8e9426172d8f22cb2a0b053fb
Ummi <i2u1@outlook.com>
2023-10-24 00:06:22 +0500

    Create a func that create an obj of album

--- piaic-course/july-2023/getting-started-typescript/src/40album.ts
+++ piaic-course/july-2023/getting-started-typescript/src/40album.ts
@@ -1,1 +1,26 @@
 #!/usr/bin/env node
+
+function makeAlbum(artist: string, title: string, tracks?: number) {
+  const album = { artist, title, ...(tracks !== undefined ? { tracks } : {}) };
+  return album;
+}
+
+const album1 = makeAlbum("Taylor Drift", "Full Space");
+const album2 = makeAlbum("Dustin Geiber", "Daddy");
+const album3 = makeAlbum("Rick Astley", "RickRoll");
+const album4 = makeAlbum("Imran Khan", "Unstoppable", 10);
+
+console.log(album1);
+console.log(album2);
+console.log(album3);
+console.log(album4);
+
+//Album: Write a function called make_album() that builds a Object describing a music album.
+//The function should take in an artist name and an album title,
+//and it should return a Object containing these two pieces of information.
+//Use the function to make three dictionaries representing different albums.
+//Print each return value to show that Objects are storing the album information correctly.
+//Add an optional parameter to make_album() that allows you to store the number of
+//tracks on an album. If the calling line includes a value for the number of tracks,
+//add that value to the album’s Object. Make at least one new function call that
+//includes the number of tracks on an album.
