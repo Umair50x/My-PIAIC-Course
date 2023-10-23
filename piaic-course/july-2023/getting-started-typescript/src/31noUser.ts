d605f1e1197f5ae278a3d883612c0b80be2be6d6
Ummi <i2u1@outlook.com>
2023-10-23 22:16:43 +0500

    Check the array for emptiness

--- piaic-course/july-2023/getting-started-typescript/src/31noUser.ts
+++ piaic-course/july-2023/getting-started-typescript/src/31noUser.ts
@@ -1,1 +1,13 @@
 #!/usr/bin/env node
+
+// import { userArr } from "./30helloAdmin.js";
+const userArr: string[] = [];
+
+if (userArr.length === 0) {
+    console.log("No Users! We need more users.");
+}
+
+
+// No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+// • Remove all of the usernames from your array, and make sure the correct message is printed.
