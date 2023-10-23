cb924460b8b153d78507b1df313c9e07b7b07697
Ummi <i2u1@outlook.com>
2023-10-23 21:40:34 +0500

    Add simple var and an if condition

--- piaic-course/july-2023/getting-started-typescript/src/25aliencolors1.ts
+++ piaic-course/july-2023/getting-started-typescript/src/25aliencolors1.ts
@@ -1,1 +1,15 @@
 #!/usr/bin/env node
+
+let alien_color = "green";
+
+if (alien_color == "green") {
+  console.log("You earned 5 points!");
+} else if (alien_color == "red") {
+}
+
+//Alien Colors #1: Imagine an alien was just shot down in a game.
+//Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+//• Write an if statement to test whether the alien’s color is green.
+//If it is, print a message that the player just earned 5 points.
+//• Write one version of this program that passes the if test and another that fails.
+//(The version that fails will have no output.)
