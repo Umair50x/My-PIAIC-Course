b5798cf4113392bd81faf353e50fb0fa3e75e2e1
Ummi <i2u1@outlook.com>
2023-10-24 01:31:48 +0500

    Add your own array and print it. simple

--- piaic-course/july-2023/getting-started-typescript/src/13yourOwnArr.ts
+++ piaic-course/july-2023/getting-started-typescript/src/13yourOwnArr.ts
@@ -1,13 +1,10 @@
 #!/usr/bin/env node
 
-
+let ownArray = ["Jet", "Car"];
+console.log("I would like to own a " + ownArray[0]);
+console.log("I would like to own a " + ownArray[1]);
 
 //Your Own Array: Think of your favorite mode of transportation,
 //such as a motorcycle or a car, and make a list that stores several examples.
 //Use your list to print a series of statements about these items,
 //such as “I would like to own a Honda motorcycle.”
-
-let ownArray = ["Jet", "Car"];
-//I don't understand this assignment due the wording used. So I'd instead do a simple print.
-console.log("I would like to own a " + ownArray[0]);
-console.log("I would like to own a " + ownArray[1]);
