61c9e95085f6f376e5f5f8a7a19171cbd91593e8
Ummi <i2u1@outlook.com>
2024-02-21 21:27:01 +0500

    Add shadcn minimal navigation and  Avatar

--- piaic-course/projects/next-zero/cloud-simple-photo-app/src/app/layout.tsx
+++ piaic-course/projects/next-zero/cloud-simple-photo-app/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="border-b ">
+          <div className="flex h-16 items-center px-4 container mx-auto">
+             Cloudinary Photos Application
+            <div className="ml-auto flex items-center space-x-4">
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            </div>
+          </div>
+        </div>
+
+        {children}</body>
     </html>
   )
 }
