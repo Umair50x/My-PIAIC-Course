20040581328630b3a48581b99fe59bd27af22197
Ummi <i2u1@outlook.com>
2024-02-27 08:15:47 +0500

    Add cloudinary search and render cld Images

--- piaic-course/projects/next-zero/cloud-simple-photo-app/src/app/gallery/page.tsx
+++ piaic-course/projects/next-zero/cloud-simple-photo-app/src/app/gallery/page.tsx
@@ -1,11 +1,39 @@
+import { RenderCldImage } from "./renderCldImage";
 import UploadButton from "./uploadButton"
+import cloudinary from "cloudinary"
+
+type searchResult= {
+    public_id: string;
+}
 
 export default async function Gallery() {
+
+    const results = await (cloudinary.v2.search
+    .expression('resource_type:image')
+    .sort_by('public_id','desc')
+    .max_results(10)
+    .execute()) as {resources: searchResult[]};
+
+    // console.log(results);
+
     return(
         <section>
             <div className="flex justify-between pt-5">
-            <h1 className="text-4xl font-bold">Gallery</h1>
-            <UploadButton />
+                <h1 className="text-4xl font-bold">Gallery</h1>
+                <UploadButton />
+                <div className="grid grid-cols-4 gap-4">
+                    {results.resources.map((result) => (
+                        <div key={result.public_id}>
+                            <RenderCldImage 
+                                width="760"
+                                height="600"
+                                src= {result.public_id}
+                                sizes="50vw"
+                                alt="Sample Description of my image"
+                            />
+                        </div>
+                    ))}
+                </div>
             </div>
         </section>
     )
